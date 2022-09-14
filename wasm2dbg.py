38a77c30fb7393f23ef51b4ebc5d7d8f01b49ef5
zode <zhangdi.zode@bytedance.com>
2022-09-14 14:00:08 +0800

    update black code formatter

--- wasm2dbg.py
+++ wasm2dbg.py
@@ -7,32 +7,41 @@ Author:   Volodymyr Shymanskyy
 Created:  18.06.2020
 """
 
-import re
-import tempfile
 import os
+import tempfile
+
 from shutil import which
 
 infile = "test_app_c/app.wasm"
 outfile = infile + ".dbg"
 
+
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
+
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
+
 sections = [
     "CODE",
-    ".debug_info", ".debug_macinfo", ".debug_ranges",
-    ".debug_abbrev", ".debug_line", ".debug_str"
+    ".debug_info",
+    ".debug_macinfo",
+    ".debug_ranges",
+    ".debug_abbrev",
+    ".debug_line",
+    ".debug_str",
 ]
 
-utils = dotdict({
-    "objdump": "llvm-objdump-10",
-    "objcopy": "objcopy",
-    "tail":    "tail",
-    "xxd":     "xxd",
-})
+utils = dotdict(
+    {
+        "objdump": "llvm-objdump-10",
+        "objcopy": "objcopy",
+        "tail": "tail",
+        "xxd": "xxd",
+    }
+)
 
 for name, exe in utils.items():
     if not which(exe):
@@ -42,14 +51,18 @@ tmp = tempfile.TemporaryDirectory()
 
 # Extract DWARF info from WASM file
 for sect in sections:
-    os.system(f"{utils.objdump} -s --section {sect} {infile} | {utils.tail} -n +5 | {utils.xxd} -r > {tmp.name}/section-{sect}")
+    os.system(
+        f"{utils.objdump} -s --section {sect} {infile} | {utils.tail} -n +5 |"
+        f" {utils.xxd} -r > {tmp.name}/section-{sect}"
+    )
 
 # Create a dummy binary file
-with open(f"{tmp.name}/empty.bin", 'w') as f:
-     f.write("wasm")
+with open(f"{tmp.name}/empty.bin", "w") as f:
+    f.write("wasm")
 
 # Create an ELF container with DWARF info
-os.system(f"""
+os.system(
+    f"""
 {utils.objcopy} -I binary -O elf32-i386 -B i386 \
   --add-section .debug_str={tmp.name}/section-.debug_str \
   --add-section .debug_line={tmp.name}/section-.debug_line \
@@ -59,4 +72,5 @@ os.system(f"""
   --add-section .debug_info={tmp.name}/section-.debug_info \
   --add-section .text={tmp.name}/section-CODE \
   {tmp.name}/empty.bin {outfile}
-""")
+"""
+)
